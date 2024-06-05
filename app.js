61b3c680f4357cc8a0fcd317df43cddfe2c6d62e
zackhamin <ishaqhamin@gmail.com>
2024-06-05 12:20:52 +0100

    chore(file): add files

---
+++ app.js
@@ -0,0 +1,17 @@
+//import the customers data
+//import the product data
+
+// We want to get a list of all customers under 40
+
+// We want all the customers who have an email address
+
+//Filter out products that are out of stock
+
+//? FizzBuzz
+// Write a short program that prints each number from 1 to 100 on a new line.
+
+// For each multiple of 3, print "Fizz" instead of the number.
+
+// For each multiple of 5, print "Buzz" instead of the number.
+
+// For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
---
+++ data/customers.js
@@ -0,0 +1,77 @@
+export const customers = [
+  {
+    name: "George Summers",
+    age: 65,
+    email: "george@summers.com",
+  },
+  {
+    name: "Emily Johnson",
+    age: 29,
+    email: "emily.johnson@example.com",
+  },
+  {
+    name: "Michael Brown",
+    age: 42,
+    email: null,
+  },
+  {
+    name: "Sophia Williams",
+    age: 34,
+    email: "sophia.williams@example.com",
+  },
+  {
+    name: "James Davis",
+    age: 51,
+    email: "james.davis@example.com",
+  },
+  {
+    name: "Olivia Martinez",
+    age: 27,
+    email: "olivia.martinez@example.com",
+  },
+  {
+    name: "William Garcia",
+    age: 38,
+    email: "william.garcia@example.com",
+  },
+  {
+    name: "Isabella Rodriguez",
+    age: 45,
+    email: "isabella.rodriguez@example.com",
+  },
+  {
+    name: "Jacob Miller",
+    age: 53,
+    email: null,
+  },
+  {
+    name: "Mia Hernandez",
+    age: 30,
+    email: "mia.hernandez@example.com",
+  },
+  {
+    name: "Ethan Wilson",
+    age: 37,
+    email: "ethan.wilson@example.com",
+  },
+  {
+    name: "Ava Moore",
+    age: 41,
+    email: "ava.moore@example.com",
+  },
+  {
+    name: "Alexander Taylor",
+    age: 48,
+    email: "alexander.taylor@example.com",
+  },
+  {
+    name: "Charlotte Anderson",
+    age: 33,
+    email: null,
+  },
+  {
+    name: "Daniel Thomas",
+    age: 36,
+    email: "daniel.thomas@example.com",
+  },
+];
